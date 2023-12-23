822f40f24896d162b0691c9713c4f6ae3a5dbaa4
limyide123 <153822617+limyide123@users.noreply.github.com>
2023-12-23 12:10:56 +0800

    Quiz data

---
+++ quiz_data.py
@@ -0,0 +1,202 @@
+quiz_data = [
+    {
+        "question": "How old is Cyun?",
+        "choices": ["A. 12", "B. 17", "C. 18", "D. bro is eternal"],
+        "answer": "C. 18"
+    },
+    {
+        "question": "When did Malaysia gain its independence?",
+        "choices": ["A. 1954", "B. 1970", "C. 1957", "D. 1980"],
+        "answer": "C. 1957"
+    },
+    {
+        "question": "Which animal lays the largest eggs?",
+        "choices": ["A. Whale", "B. Crocodile", "C. Elephant", "D. Ostrich"],
+        "answer": "D. Ostrich"
+    },
+    {
+        "question": "How many bones are in the human body?",
+        "choices": ["A. 206", "B. 207", "C. 208", "D. 209"],
+        "answer": "A. 206"
+    },
+    {
+        "question": "Which planet in the solar system is the largest?",
+        "choices": ["A. Earth", "B. Jupiter", "C. Saturn", "D. Mars"],
+        "answer": "B. Jupiter"
+    },
+    {
+        "question": "What is the longest word in the English language?",
+        "choices": ["A. Antidisestablishmentarianism", "B. Hippopotomonstrosesquippedaliophobia", "C. Floccinaucinihilipilification", "D. Raziqisveryhandsome"],
+        "answer": "A. Antidisestablishmentarianism"
+    },
+    {
+        "question": "What is the name of the world’s smallest horse?",
+        "choices": ["A. Falabella", "B. Shetland pony", "C. Miniature horse", "D. My little pony"],
+        "answer": "C. Miniature horse"
+    },
+    {
+        "question": "What is Benedictine monk Dom Pierre Pérignon rumored to have created?",
+        "choices": ["A. Tomato ketchup", "B. Champagne", "C. French fries", "D. Chili sauce"],
+        "answer": "B. Champagne"
+    },
+    {
+        "question": "Which country drinks the most amount of coffee per person?",
+        "choices": ["A. Finland", "B. Italy", "C. Colombia", "D. Malaysia"],
+        "answer": "A. Finland"
+    },
+    {
+        "question": "What is the collective name for a group of unicorns?",
+        "choices": ["A. A sparkle", "B. A spell", "C. A herd", "D. A blessing"],
+        "answer": "D. A blessing"
+    },
+    {
+        "question": "What is the most common color of toilet paper in France?",
+        "choices": ["A. Pink", "B. White", "C. Blue", "D. Rainbow"],
+        "answer": "A. Pink"
+    },
+    {
+        "question": "How many years old is the world’s oldest piece of chewing gum?",
+        "choices": ["A. 100", "B. 2,500", "C. 4,000", "D. 5,700"],
+        "answer": "D. 5,700"
+    },
+    {
+        "question": "How many times per day does the average American open their fridge?",
+        "choices": ["A. 5", "B. 22", "C. 33", "D. 50"],
+        "answer": "C. 33"
+    },
+    {
+        "question": "What color is an airplane’s famous black box?",
+        "choices": ["A. Red", "B. Orange", "C. Black", "D. Yellow"],
+        "answer": "B. Orange"
+    },
+    {
+        "question": "What is Bombay Duck’s main ingredient?",
+        "choices": ["A. Fish", "B. Duck", "C. Chicken", "D. Curry"],
+        "answer": "A. Fish"
+    },
+    {
+        "question": "How many tails does a Manx cat have?",
+        "choices": ["A. None", "B. One", "C. Two", "D. Three"],
+        "answer": "A. None"
+    },
+    {
+        "question": "As of July 2023, how many episodes of South Park are there?",
+        "choices": ["A. 250", "B. 300", "C. 325", "D. 400"],
+        "answer": "C. 325"
+    },
+    {
+        "question": "In what decade was Madonna born?",
+        "choices": ["A. 1950s", "B. 1960s", "C. 1970s", "D. 1980s"],
+        "answer": "A. 1950s"
+    },
+    {
+        "question": "In what language is the phrase ‘Hakuna Matata’?",
+        "choices": ["A. Dutch", "B. Swahili", "C. Yoruba", "D. German"],
+        "answer": "B. Swahili"
+    },
+    {
+        "question": "And what is the meaning of ‘Hakuna Matata’?",
+        "choices": ["A. No worries", "B. Goodnight", "C. Thank you", "D. Who cares?"],
+        "answer": "A. No worries"
+    },
+    {
+        "question": "What is the name of a duel with three people involved?",
+        "choices": ["A. A triage", "B. A truel", "C. A tryst", "D. A triangle"],
+        "answer": "B. A truel"
+    },
+    {
+        "question": "How many stars are on the United States flag?",
+        "choices": ["A. 50", "B. 51", "C. 52", "D. 53"],
+        "answer": "A. 50"
+    },
+    {
+        "question": "In which year was Slido founded?",
+        "choices": ["A. 2012", "B. 2016", "C. 2020", "D. 2021"],
+        "answer": "A. 2012"
+    },
+    {
+        "question": "Who is credited with inventing the World Wide Web?",
+        "choices": ["A. Steve Jobs", "B. Bill Gates", "C. Tim Berners-Lee", "D. Gay-Lussac"],
+        "answer": "C. Tim Berners-Lee"
+    },
+    {
+        "question": "What type of computer was the first laptop computer?",
+        "choices": ["A. Apple Macintosh", "B. IBM PC", "C. Osborne 1", "D. Lenovo"],
+        "answer": "C. Osborne 1"
+    },
+    {
+        "question": "What is the largest social media network in the world?",
+        "choices": ["A. Twitter", "B. Facebook", "C. Instagram", "D. TikTok"],
+        "answer": "B. Facebook"
+    },
+    {
+        "question": "Who is considered the founder of modern computer science?",
+        "choices": ["A. Alan Turing", "B. Steve Jobs", "C. Bill Gates", "D. Isaac Newton"],
+        "answer": "A. Alan Turing"
+    },
+    {
+        "question": "What year was the iPhone first released in?",
+        "choices": ["A. 2007", "B. 2009", "C. 2010", "D. 2011"],
+        "answer": "A. 2007"
+    },
+    {
+        "question": "Which video game console, first released in 2006, was the first to use motion controls during gameplay?",
+        "choices": ["A. Sega Megadrive", "B. Nintendo Wii", "C. Playstation", "D. Sony"],
+        "answer": "B. Nintendo Wii"
+    },
+    {
+        "question": "When was eBay founded?",
+        "choices": ["A. 1990", "B. 1995", "C. 1998", "D. 2001"],
+        "answer": "B. 1995"
+    },
+    {
+        "question": "A green owl is the mascot for which app?",
+        "choices": ["A. Spotify", "B. Tinder", "C. YouTube", "D. Duolingo"],
+        "answer": "D. Duolingo"
+    },
+    {
+        "question": "In which year did the Berlin Wall fall?",
+        "choices": ["A. 1987", "B. 1989", "C. 1990", "D. 1995"],
+        "answer": "B. 1989"
+    },
+    {
+        "question": "How many times has the Mona Lisa been stolen?",
+        "choices": ["A. One", "B. Five", "C. Eight", "D. Ten"],
+        "answer": "A. One"
+    },
+    {
+        "question": "In Ancient Rome, how many days of the week were there?",
+        "choices": ["A. Five", "B. Six", "C. Seven", "D. Eight"],
+        "answer": "D. Eight"
+    },
+    {
+        "question": "What was New York’s original name?",
+        "choices": ["A. New Liverpool", "B. New Amsterdam", "C. New Brussels", "D. New World"],
+        "answer": "B. New Amsterdam"
+    },
+    {
+        "question": "In what year did the Titanic sink?",
+        "choices": ["A. 1900", "B. 1912", "C. 1921", "D. 1932"],
+        "answer": "B. 1912"
+    },
+    {
+        "question": "Until 1981, Greenland was a colony of which country?",
+        "choices": ["A. France", "B. Spain", "C. German", "D. Denmark"],
+        "answer": "D. Denmark"
+    },
+    {
+        "question": "How many US presidents have been assassinated?",
+        "choices": ["A. Three", "B. Four", "C. Five", "D. Six"],
+        "answer": "B. Four"
+    },
+    {
+        "question": "Who was assassinated in New York in 1980?",
+        "choices": ["A. President John F Kennedy", "B. John Lennon", "C. Gianni Versace", "D. Abraham Lincoln"],
+        "answer": "B. John Lennon"
+    },
+    {
+        "question": "Who painted The Last Supper?",
+        "choices": ["A. Leonardo Da Vinci", "B. Rembrandt", "C. Michelangelo", "D. Mona Lisa"],
+        "answer": "A. Leonardo Da Vinci"
+    },
+]
